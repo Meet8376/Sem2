97979d3aed94df7b3f92c646c5360a4026c9c1e0
Meet8376 <agrawalmeet11@gmail.com>
2026-02-11 13:54:06 +0530

    dknck

--- sjf.c
+++ sjf.c
@@ -1,45 +1,62 @@
 #include<stdio.h>
-int main() {
-    int n,i,j;
-    int bt[20],p[20],wt[20],tat[20];
-    float avg_wt=0,avg_tat=0;
-    int temp;
+#include<stdlib.h>
+#define MAX 50
+struct Process
+{
+    int pid,at,bt,wt,ct,rt,tat;
+}P[MAX];
+int main()
+{
+    int i,j,n;
     printf("Enter number of processes: ");
-    scanf("%d",&n);
-    for(i=0;i<n;i++) {
-        p[i]=i+1;
-        printf("Enter Burst Time for P%d: ",i+1);
-        scanf("%d",&bt[i]);
+    scanf("%d", &n);
+    for(i=0;i<n;i++)
+    {
+        printf("Enter PID, AT and BT of the process : ");
+        scanf("%d %d %d",&P[i].pid,&P[i].at,&P[i].bt);
     }
-    for(i=0;i<n;i++) {
-        for(j=i+1;j<n;j++) {
-            if(bt[i]>bt[j]) {
-                temp=bt[i];
-                bt[i]=bt[j];
-                bt[j]=temp;
-                temp=p[i];
-                p[i]=p[j];
-                p[j]=temp;
+
+    // Sort by AT and BT
+    for(i=0;i<n-1;i++)
+    {
+        for(j=0;j<n-1-i;j++)
+        {
+            if(P[j].at > P[j+1].at || (P[j].at == P[j+1].at && P[j].bt > P[j+1].bt))
+            {
+                struct Process temp = P[j];
+                P[j] = P[j+1];
+                P[j+1] = temp;
             }
         }
     }
-    wt[0]=0;
-    for(i=1;i<n;i++) {
-        wt[i]=wt[i-1]+bt[i-1];
+
+    int current_time = 0;
+    for(i=0;i<n;i++) // CT 
+    {
+        if(current_time < P[i].at)
+            current_time = P[i].at;
+        current_time += P[i].bt;
+        P[i].ct = current_time;
     }
-    for(i=0;i<n;i++) {
-        tat[i]=wt[i]+bt[i];
-        avg_wt+=wt[i];
-        avg_tat+=tat[i];
+
+    for(i=0;i<n;i++)
+    {
+        P[i].tat = P[i].ct - P[i].at;
+        P[i].wt = P[i].tat - P[i].bt;
+        P[i].rt = P[i].wt;
     }
-    avg_wt/=n;
-    avg_tat/=n;
-    printf("\nProcess\tBurst Time\tWaiting Time\tTurnaround Time");
-    for(i=0;i<n;i++) {
-        printf("\nP%d\t%d\t\t%d\t\t%d",p[i],bt[i],wt[i],tat[i]);
+
+    printf("PID\tAT\tBT\tCT\tTAT\tWT\tRT\n");
+    for(i=0;i<n;i++)
+    {
+        printf("%d\t%d\t%d\t%d\t%d\t%d\t%d\n",P[i].pid,P[i].at,P[i].bt,P[i].ct,P[i].tat,P[i].wt,P[i].rt);
     }
-    printf("\n\nAverage Waiting Time=%f",avg_wt);
-    printf("\nAverage Turnaround Time=%f",avg_tat);
-    //dsbibvkjdsnlk
+
+    printf("GANTTCHART\n[ ");
+    for(i=0;i<n;i++)
+    {
+        printf("P%d ",P[i].pid);
+    }
+    printf("]\n");
     return 0;
 }
